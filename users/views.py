41300843b45ddd6fbba7ec01a4f30e470b038ccb
xoxowo <1qq11212@gmail.com>
2023-03-02 23:25:26 +0900

    views code convention modify

--- users/views.py
+++ users/views.py
@@ -27,7 +27,7 @@ class JoinView(View):
             validate_password(password)
 
             if User.objects.filter(username = username).exists():
-                return JsonResponse({'MESSAGE':'Already_Registered_User'}, status=400) 
+                return JsonResponse({'Message':'Already_Registered_User'}, status=400) 
 
             hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
@@ -39,16 +39,16 @@ class JoinView(View):
                 email        = email,
                 point        = DEFAULT_POINT,
             )
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+            return JsonResponse({'Message':'Success'}, status=201)
         
         except ValidationError as error:
-            return JsonResponse({'MESSAGE':error.message}, status = 400)
+            return JsonResponse({'Message':error.message}, status = 400)
 
         except json.JSONDecodeError:
-            return JsonResponse({'MESSAGE':'JSONDecodeError'}, status=400)           
+            return JsonResponse({'Message':'JSONDecodeError'}, status=400)           
 
         except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+            return JsonResponse({'Message':'Key_Error'}, status=400)
 
 class LoginView(View):
     def post(self, request):
@@ -61,14 +61,14 @@ class LoginView(View):
             user = User.objects.get(username = username)
 
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+                return JsonResponse({'MESSAGE':'Invalid_User'}, status=401)
         
             access_token = jwt.encode({'id': user.id}, settings.SECRET_KEY, settings.ALGORITHM)
 
-            return JsonResponse({'MESSAGE':'SUCCESS', 'AUTHORIZATION':access_token}, status=200)
+            return JsonResponse({'Message':'Success', 'Authorization':access_token}, status=200)
 
         except User.DoesNotExist:
-            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+            return JsonResponse({'Message':'Invalid_User'}, status=401)
 
         except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)        
+            return JsonResponse({'Message':'Key_Error'}, status=400)        
